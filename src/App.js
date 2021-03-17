b6dec34f27df1f56b260c5a51b05b5263192018b
Naveen Varma <naveenv802@gmail.com>
2021-03-17 18:16:17 -0400

    React assignment using login component - taking username and password as input and signin button - displaying welcome message with user name and signout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react';
+import LoginForm from './LoginComponent'
+const Welcome = ({user, onSignOut})=> {
+  // This is a dumb "stateless" component
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Welcome <strong>{user.username}</strong>!
+      <a href="javascript:;" onClick={onSignOut}>Sign out</a>
     </div>
-  );
+  )
 }
+
+
+class App extends React.Component {
+  
+  constructor(props) {
+    super(props)
+    // the initial application state
+    this.state = {
+      user: null
+    }
+  }
+  
+  signIn(username, password) {
+    this.setState({
+      user: {
+        username,
+        password,
+      }
+    })
+  }
+  
+  signOut() {
+    this.setState({user: null})
+  }
+  
+  render() {
+    return (
+      <div>
+        <h1>Login Component using form</h1>
+        { 
+          (this.state.user) ? 
+            <Welcome 
+             user={this.state.user} 
+             onSignOut={this.signOut.bind(this)} 
+            />
+          :
+            <LoginForm 
+             onSignIn={this.signIn.bind(this)} 
+            />
+        }
+      </div>
+    )
+    
+  }
+  
+}
+
 
 export default App;
